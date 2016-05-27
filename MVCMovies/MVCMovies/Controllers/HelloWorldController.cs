2f5a49189f6ebd26b98e582cf57073c00f7c068f
petak <martina.petak@fer.hr>
2016-05-27 17:48:23 +0200

    model + database

--- MVCMovies/MVCMovies/Controllers/HelloWorldController.cs
+++ MVCMovies/MVCMovies/Controllers/HelloWorldController.cs
@@ -18,9 +18,12 @@ namespace MVCMovies.Controllers
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.numTimes = numTimes;
+            // return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            return View();
         }
 	}
 }
